ac952d1c6aeda6ebd328a1058eb360156d52e91d
Ubuntu <ubuntu@ip-172-31-40-150.ap-south-1.compute.internal>
2019-08-23 06:26:03 +0000

    fix applied in sync peers

--- sync_peers.sh
+++ sync_peers.sh
@@ -8,13 +8,31 @@ FABRIC_CFG_PATH=/opt/gopath/src/github.com/hyperledger/fabric/sampleconfig
 export CORE_PEER_TLS_ROOTCERT_FILE=`pwd`/crypto-config/peerOrganizations/${DOMAIN}/peers/${bootPeer}.${DOMAIN}/tls/ca.crt
 export CORE_PEER_TLS_ENABLED=true
 
+PEER_MSPID=PeerOrg
+PEER_MSPCONFIG=`pwd`/crypto-config/peerOrganizations/${DOMAIN}/users/Admin@${DOMAIN}/msp/
+CORE_PEER_MSPCONFIGPATH=`pwd`/crypto-config/peerOrganizations/${DOMAIN}/users/Admin@${DOMAIN}/msp
+
+CHAINCODE_ARGS='{"Args":["query","a"]}'
+CHANNEL_NAME=yacov
+CHAINCODE_NAME=exampleCC
+ORDERER_CA_FILE=`pwd`/crypto-config/ordererOrganizations/${DOMAIN}/orderers/${orderer}.${DOMAIN}/tls/ca.crt
+
+query() {
+    CORE_PEER_LOCALMSPID=${PEER_MSPID} \
+    CORE_PEER_MSPCONFIGPATH=${CORE_PEER_MSPCONFIGPATH} \
+    CORE_PEER_ADDRESS=$1:7051 \
+    ./peer chaincode query -c ${CHAINCODE_ARGS} -C ${CHANNEL_NAME} -n ${CHAINCODE_NAME} \
+    --tls true \
+    --cafile ${ORDERER_CA_FILE}
+}
+
 echo "Waiting for peers $peers to sync..."
 t1=`date +%s`
 while :; do
 	allInSync=true
 	for p in ${peers} ; do
 	    echo "Querying $p..."
-	    query $p | grep -q 'Query Result: 50'
+	    echo "Query Result: $(query ${p})"
 	    if [[ $? -ne 0 ]];then
 		    allInSync=false
 	    fi
@@ -28,3 +46,4 @@ done
 echo "All peers are now in sync"
 
 exit 0
+
